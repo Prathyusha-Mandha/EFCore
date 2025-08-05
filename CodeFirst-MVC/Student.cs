872c3e43879b205cc4c1fedb941100ac5454af63
Prathyusha-Mandha <127721477+Prathyusha-Mandha@users.noreply.github.com>
2025-08-05 18:13:42 +0530

    Add files via upload

---
+++ CodeFirst-MVC/Student.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeFirst_Practice.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StuId { get; set; }
+
+        public string StudentName { get; set; }
+
+        public decimal Telugu { get; set; }
+
+        public decimal Hindi { get; set; }
+
+        public decimal English { get; set; }
+
+        public decimal Maths { get; set; }
+
+        public decimal Science { get; set; }
+
+        public decimal Social { get; set; }
+
+        
+
+
+    }
+}
---
+++ CodeFirst-MVC/StudentContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirst_Practice.Models
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=Prathyusha;database=CodeFirst;integrated security=True;trustservercertificate=True;");
+        }
+    }
+}
