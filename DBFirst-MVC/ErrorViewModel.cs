536d3e94f36c21287d72e5332a619c177ec429f5
Prathyusha-Mandha <127721477+Prathyusha-Mandha@users.noreply.github.com>
2025-08-05 18:09:03 +0530

    Add files via upload

---
+++ DBFirst-MVC/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DB_first_Practice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ DBFirst-MVC/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DB_first_Practice.Models;
+
+public partial class Student
+{
+    public int StuId { get; set; }
+
+    public string StudentName { get; set; } = null!;
+
+    public decimal Telugu { get; set; }
+
+    public decimal Hindi { get; set; }
+
+    public decimal English { get; set; }
+
+    public decimal Maths { get; set; }
+
+    public decimal Science { get; set; }
+
+    public decimal Social { get; set; }
+}
