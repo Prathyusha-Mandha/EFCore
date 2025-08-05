ec63b8541b9f71276fd181e9e670ca02981258f9
Prathyusha-Mandha <127721477+Prathyusha-Mandha@users.noreply.github.com>
2025-08-05 18:22:19 +0530

    Add files via upload

---
+++ DBFirst-Console/CodeFirstContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DB_First_Console_Practice.Models;
+
+public partial class CodeFirstContext : DbContext
+{
+    public CodeFirstContext()
+    {
+    }
+
+    public CodeFirstContext(DbContextOptions<CodeFirstContext> options)
+        : base(options)
+    {
+    }
+
+    
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("data source=Prathyusha;database=CodeFirst;integrated security=True;trustservercertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.StuId);
+
+            entity.Property(e => e.English).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Hindi).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Maths).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Science).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Social).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Telugu).HasColumnType("decimal(18, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DBFirst-Console/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DB_First_Console_Practice.Models;
+
+public partial class Student
+{
+    public int StuId { get; set; }
+
+    public string StudentName { get; set; } = null!;
+
+    public decimal Telugu { get; set; }
+
+    public decimal Hindi { get; set; }
+
+    public decimal English { get; set; }
+
+    public decimal Maths { get; set; }
+
+    public decimal Science { get; set; }
+
+    public decimal Social { get; set; }
+}
